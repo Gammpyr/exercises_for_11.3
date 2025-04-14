cd0d29ccef80e4451a323a5940094e7b577c3a99
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-04-14 21:48:49 +0300

    exercise4 ch4

---
+++ chapter4_decorators/exercise4(tracers).py
@@ -0,0 +1,25 @@
+import sys
+def debug_intermediate_results(func):
+    def wrapper(*args, **kwargs):
+        def trace_calls(frame, event, arg):
+            if event == 'line':
+                local_vars = frame.f_locals
+                print(f"Промежуточные результат в {func.__name__}: {local_vars}")
+            return trace_calls
+        sys.settrace(trace_calls)
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            sys.settrace(None)
+            return result
+    return wrapper
+
+# Пример использования декоратора
+@debug_intermediate_results
+def example_function(x):
+    y = x + 1
+    z = y * 2
+    return z
+
+result = example_function(5)
+print(f"Финальный результат: {result}")
