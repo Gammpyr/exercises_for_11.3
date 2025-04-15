6c5a7265b3a9be626e8449f8767b6fc62674e843
Gamid Abdulkhalikov <malak93@yandex.ru>
2025-04-15 18:35:43 +0300

    finished exercise9 ch4

--- chapter4_decorators/exercise9.py
+++ chapter4_decorators/exercise9.py
@@ -41,3 +41,41 @@ print(f(5))
 print(f(5))
 
 print(f'Затрачено времени: {time.time() - start}')
+
+
+# import functools
+#
+# def memoizing(max_cache_size):
+#     def decorator(func):
+#         cache = {}
+#         order = []
+#
+#         @functools.wraps(func)
+#         def wrapper(x):
+#             if x in cache:
+#                 return cache[x]
+#             else:
+#                 result = func(x)
+#                 if len(order) >= max_cache_size:
+#                     oldest = order.pop(0)
+#                     del cache[oldest]
+#                 cache[x] = result
+#                 order.append(x)
+#                 return result
+#         return wrapper
+#     return decorator
+#
+# # Пример использования декоратора
+#
+# @memoizing(3)
+# def f(x):
+#     print('Calculating...')
+#     return x * 10
+#
+# # Примеры вызовов функции
+# print(f(1))  # Output: Calculating... 10
+# print(f(1))  # Output: 10
+# print(f(2))  # Output: Calculating... 20
+# print(f(3))  # Output: Calculating... 30
+# print(f(4))  # Output: Calculating... 40
+# print(f(1))  # Output: Calculating... 10
